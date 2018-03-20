02a4c01a08af6037de22286bc5b9abd9a7bc6d26
Attache William <wattache@localhost.localdomain>
2018-03-20 17:16:42 +0100

    run but probably an infinite loop

--- arbenz.m
+++ arbenz.m
@@ -1,3 +1,6 @@
+% T = (diag(1:1:5) + diag(ones(4,1),1) + diag(ones(4,1),-1))
+% no deflation necessary
+
 function [Q,L] = arbenz(T)
 
 	if (length(T) == 1)
@@ -18,15 +21,14 @@ function [Q,L] = arbenz(T)
 		end
 		T1 = T(1:m,1:m);
 		bm = T(m,m+1);
-		T1(m,m) = T1(m,m) + bm
+		T1(m,m) = T1(m,m) + bm;
 		T2 = T(m+1:n,m+1:n);
-		T2(1,1) = T2(1,1) + bm
+		T2(1,1) = T2(1,1) + bm;
 		u = zeros(n,1);
 		u(m) = -1;
 		u(m+1) = 1;
 		rho = -bm;
-		% T = [T1 0 ; 0 T2] + rho*u*u'
-		
+		%T = [T1 0 ; 0 T2] + rho*u*u'
 		
 		%%%%%%%%%%%% Call of the algorithm on T1 and T2 %%%%%%%%%%%%%%%%
 		[Q1,L1] = arbenz(T1);  % Q1 is mxm
@@ -36,10 +38,11 @@ function [Q,L] = arbenz(T)
 		%%%%%%%%%%%%%%%%%%%%% Shaping of D and v %%%%%%%%%%%%%%%%%%%%%%%
 		D = zeros(n,n);
 		D(1:m,1:m) = L1;
-		D(m+1:n,m+1,n) = L2;
+		D(m+1:n,m+1:n) = L2;
 		v = zeros(n,1);
-		v(1:m) = Q1'*u(1:m);
-		v(m+1:n) = Q2'*u(m+1,n);
+		v(1:m) = (Q1')*u(1:m);
+		v(m+1:n) = (Q2')*u(m+1:n);
+		
 		
 		
 		%%%%%%%%%%%%%%%%%%%%%%% Deflation step %%%%%%%%%%%%%%%%%%%%%%%%%
@@ -57,26 +60,36 @@ function [Q,L] = arbenz(T)
 		% additionnal zero according to the sign of rho.  
 		
 		L = zeros(n-1,1);  % then we will concatenant the last zero
+		lambda = zeros(n,1);
 		
 		d = diag(D); % column vector
 		
 		for i = 1 : (n-1) 
-			L(i) = dichotomous(@(x)secular(x,v,d),d(i),d(i+1),itMax,epsilon);
+			L(i) = dichotomous(@(x)secular(x,v,d,rho),d(i),d(i+1),itMax,epsilon);
+			L(i)
 		end 
 	
 		% Computation of the additionnal eigenvalue : we use a 
 		% dichotomous search
 		lastZero = 0;
 		if (rho > 0) % search between d(n) and Inf
-			cs = changeSign(@(x)secular(x,v,d),d(n),"i");
-			lastZero = dichotomous(@(x)secular(x,v,d),d(n),cs,itMax,epsilon);
+			cs = changeSign(@(x)secular(x,v,d,rho),d(n),"i");
+			lastZero = dichotomous(@(x)secular(x,v,d,rho),d(n),cs,itMax,epsilon);
+			lambda(1:n-1) = L;
+			lambda(n) = lastZero;
 		else % search between -Inf and d(1) 
-			cs = changeSign(@(x)secular(x,v,d),d(1),"d");
-			lastZero = dichotomous(@(x)secular(x,v,d),cs,d(1),itMax,epsilon);
+			cs = changeSign(@(x)secular(x,v,d,rho),d(1),"d");
+			lastZero = dichotomous(@(x)secular(x,v,d,rho),cs,d(1),itMax,epsilon);
+			lambda(2:n) = L;
+			lambda(1) = lastZero;
 		end
 		
 		%%%%%%%%%%%%% Find the eigenvectors of D+rho*v*v' %%%%%%%%%%%%%%
 		Qp = zeros(n,n);
+		for j = 1:n
+			M = inv((lambda(j)*eye(n) - D))*v;
+			Q(:,j) = M ./ norm(M,2);
+		end
 		
 		
 		%%%%%%%%%%% Form the matrix of the eigenvectors of T %%%%%%%%%%%
@@ -101,7 +114,8 @@ end
 %			-> "d" : search to inverse the sign of f between -Inf and d
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function x = changeSign(f,d,sense):
+function d = changeSign(f,di,sense)
+	d = di;
 	step = 50;
 	converged = false;
 	while (~converged)
@@ -119,6 +133,39 @@ end
 
 
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Solve the secular equation thanks to a dichotomous search. 
+% Param : -f : the function which we search a zero.
+%		  -a : the lower bound of the research interval.
+%		  -b : the upper bound of the research interval.
+%		  -itMax : maximum number of iterations to do. 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function c = dichotomous(f,a,b,itMax,epsilon) 
+	
+	c = (a+b)/2;
+	it = 1;
+
+	while ((abs(f(c)) >= epsilon) && (it < itMax))
+		if (f(c)*f(b) <= 0)
+			a = c;
+		else 
+			b = c;
+		end 
+		c = (a+b)/2;
+		it = it + 1;
+	end 
+	
+	if (it >= itMax)
+		flag = 2;
+	else 
+		flag = 1;
+	end
+end
+
+
+
+
 
 
 
@@ -126,7 +173,7 @@ end
 % Definition of the secular function.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function y = secular(x,v,d)
+function y = secular(x,v,d,rho)
 	sum = 0;
 	for k = 1:length(d)
 		sum = sum + v(k)^2/(x-d(k));
